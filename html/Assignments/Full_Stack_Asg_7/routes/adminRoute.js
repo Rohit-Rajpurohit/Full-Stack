2523fe9e253561eaa8270f6999ffa40a43627384
Rohit-Rajpurohit <rajpurohitrohit869@gmail.com>
2021-11-02 14:19:32 +0530

    remove folder

--- html/Assignments/Full_Stack_Asg_7/routes/adminRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express();
-const adminController = require("../controller/adminController");
-
-router.post("/signup", adminController.signup);
-router.get("/login", adminController.login);
-
-module.exports = router;
